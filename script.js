abfa0f474dbe1fca83fb38dc5d4354ac4577a07e
velsanjay <sanjaymech2313@mail.com>
2023-02-09 13:21:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let a=document.querySelector('.a')
+setTimeout(()=>{
+    a.innerText='10'
+    setTimeout(()=>{
+        a.innerText='9'
+        setTimeout(()=>{
+            a.innerText='8'
+            setTimeout(()=>{
+                a.innerText='7'
+                setTimeout(()=>{
+                    a.innerText='6'
+                    setTimeout(()=>{
+                        a.innerText='5'
+                        setTimeout(()=>{
+                            a.innerText='4'
+                            setTimeout(()=>{
+                                a.innerText='3'
+                                setTimeout(()=>{
+                                    a.innerText='2'
+                                    setTimeout(()=>{
+                                        a.innerText='1'
+                                        setTimeout(()=>{
+                                            a.innerText= "  🎉 Happy Indipendance Day   🎉 "
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+        
+    },1000)
+    
+},1000)
